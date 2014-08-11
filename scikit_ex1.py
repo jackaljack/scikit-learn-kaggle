6346ee5c8433433ccfbd1d007b0aa1404419dbf3
jackaljack <jackdebidda@gmail.com>
2014-08-11 13:37:28 +0200

    improved grid search with 2 passes: coarse tuning and fine tuning. Slightly better SVM classifier performance

--- scikit_ex1.py
+++ scikit_ex1.py
@@ -13,7 +13,7 @@ from sklearn import preprocessing as pp
 from sklearn import cross_validation as cv
 from sklearn import grid_search
 import matplotlib.pyplot as plt
-from pylab import pcolor, colorbar
+from pylab import pcolor, colorbar, show
 
 ### import the data
 # test set. 40 features, 9000 samples
@@ -39,30 +39,32 @@ R = np.corrcoef(X_train.transpose())
 plt.figure(2)
 pcolor(R)
 colorbar()
+show()
 
-### dimensionality reduction using PCA (with whitening)
-# variance explained by all the 40 features (with whitening)
-pca40 = PCA(n_components=40, whiten=True) 
-pca40.fit(X_train)
-print(pca40.explained_variance_ratio_)
+### dimensionality reduction using PCA
 # since data is uncorrelated and with variance almost equal to 1,
 # whitening is not necessary
+pca40 = PCA(n_components=40, whiten=False) 
+pca40.fit(X_train)
+print(pca40.explained_variance_ratio_)
 
 # plot all the principal components with their relative explained variance
 features = [x for x in range(1,41)]
 plt.figure(3)
 # percentage of variance explained by each of the selected components.
 # The sum of explained variances is equal to 1.0
-plt.plot(features, pca40.explained_variance_ratio_, 'r--', marker='o')
+plt.plot(features, pca40.explained_variance_ratio_, 'g--', marker='o')
 plt.axis([1, 40, 0, 0.3])
 plt.grid(True)
 plt.xlabel("principal components"), plt.ylabel("variance explained")
 plt.title("scree plot")
 
-### standardize the variables before performing PCA
-# center to the mean and component wise scale to unit variance
-X_train = pp.scale(X_train)
-X_test = pp.scale(X_test)
+### center the variables before performing PCA
+# center to the mean, but DO NOT component wise scale to unit variance
+# by centering the variables, principal components remain the same,
+# by standardizing the variables, principal components change
+X_train = pp.scale(X_train, with_mean=True, with_std=False)
+X_test = pp.scale(X_test, with_mean=True, with_std=False)
 
 # from the scree plot we choose to pick the first 12 principal components
 pca12 = PCA(n_components=12, whiten=True) 
@@ -87,7 +89,7 @@ def qq_plot(x):
     plt.figure()
     probplot(x, dist="norm", plot=plt)
 
-#• kernel density plot and qq plot of the first principal component    
+# kernel density plot and qq plot of the first principal component    
 kde_plot(X_pca_train[:, 0])
 qq_plot(X_pca_train[:,0])
 
@@ -99,15 +101,15 @@ skf = cv.StratifiedKFold(y, n_folds=3)
 
 ### Hyperparameter optimization using grid-search
 # find C and gamma with a "grid-search" using cross validation
+# Grid Search, first pass: coarse tuning of the parameters C and gamma
 # C is a regularization parameter
 # large C makes constraints hard to ignore -> narrow separation margin
-C_range = 10.0 ** np.arange(7, 9) # 1, 10
+C_range = 10.0 ** np.arange(5, 10) # 5, 10
 # gamma is th width of the Radial Basis Function (RBF) kernel.
 # gamma controls the shape of the separating hyperplane
 gamma_range = 10.0 ** np.arange(-5, 0) # -5, 0
 # Dictionary with parameters names as keys and lists of parameter settings to try as values
 params = dict(gamma = gamma_range, C = C_range)
-
 classifier = svm.SVC(kernel='rbf')
 # GridSearchCV parameters of the classifier used to predict is optimized by cross-validation
 clf = grid_search.GridSearchCV(classifier, param_grid = params, cv = skf)
@@ -115,7 +117,14 @@ clf = grid_search.GridSearchCV(classifier, param_grid = params, cv = skf)
 clf.fit(X_pca_train, y)
 print("The best classifier is: ", clf.best_estimator_)
 
-# we can improve the classifier by performing another grid-search...
+# Grid Search, second pass: fine tuning of the parameters C and gamma
+C_range = 10.0 ** np.arange(6.5, 7.5, 0.1) 
+gamma_range = 10.0 ** np.arange(-1.5, 0.5, 0.1) 
+params = dict(gamma = gamma_range, C = C_range)
+classifier = svm.SVC(kernel='rbf')
+clf = grid_search.GridSearchCV(classifier, param_grid = params, cv = skf)
+clf.fit(X_pca_train, y)
+print("The best classifier is: ", clf.best_estimator_)
 
 # Estimate score of the classifier
 scores = cv.cross_val_score(clf.best_estimator_, X_pca_train, y, cv=10)
